a4e41a7bbd6a5f723539d36f4b6a1c3f79690a34
Theodore R. Smith <theodore.smith@bitbasket.co>
2024-11-17 02:51:52 -0600

    Initial v0.90.

---
+++ chroot-installer.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+echo "Hello, Arch Linux!"
+
+echo "Set password for root: "
+passwd
+
+read -p "Username for local admin? " user
+useradd -m $user -G adm,wheel,users
+
+echo "Set password for $user: "
+passwd $user
+
+# Enable the en_US locales
+sed -i s/#en_US/en_US/ /etc/locale.gen
+locale-gen
+
+
+pacman -Syu
+pacman -S --noconfirm git vim sudo docker xfsprogs btrfs-progs mdadm linux-lts \
+                      openssh grub efibootmgr dmidecode gdisk dosfstools net-tools \
+                      ack
+systemctl enable sshd
+
+echo "Generate SSH key for root:"
+ssh-keygen -t ed25519
+
+echo "Generate SSH for the local admin ($user)..."
+echo "Make sure you set a password, as this key will also be used for sudo auth..."
+sudo -u $user ssh-keygen -t ed25519
+
+genfstab -U / > /etc/fstab
+
+mkdir -p /boot/grub
+grub-mkconfig > /boot/grub/grub.cfg
+
+# Install grub:
+if efibootmgr > /dev/null; then 
+    echo "Installing Grub for UEFI..."; 
+else
+    echo "Setting up the 1 MiB BIOS boot partition..."
+    echo "Step 1: Backup the parititon table..."
+    sgdisk --backup=partition-table-backup.gpt /dev/nvme0n1
+    echo "Step 2: Delete and recreate partition 1..."
+    umount /dev/nvme0n1p1
+    sgdisk --delete=1 /dev/nvme0n1
+    sgdisk --new=1:2048:+8192s --typecode=1:EF00 --change-name=1:"EFI system partition" /dev/nvme0n1
+    echo "Step 3: Create BIOS Boot Partition..."
+    sgdisk --new=6:10240:+2048s --typecode=6:21686148-6449-6E6F-744E-656564454649 --change-name=6:"BIOS boot partition" /dev/nvme0n1
+
+    echo "Step 4: Recreate the file system on partition 1..."
+    mkfs.fat -F32 /dev/nvme0n1p1
+
+    echo "Step 5: Regenerate /etc/fstab..."
+    genfstab -U / > /etc/fstab 
+
+    echo "Step 6: Install Grub for BIOS...";
+    grub-install --target=i386-pc /dev/nvme0n1
+fi
+
+echo "Enabling btrfs and mdadm in Grub boots..."
+sed -i 's/^MODULES=()/MODULES=(btrfs)/' /etc/mkinitcpio.conf
+sed -i 's/^BINARIES=()/BINARIES=(\/usr\/bin\/btrfs)/' /etc/mkinitcpio.conf
+sed -i 's/^HOOKS=(base udev/HOOKS=(base udev mdadm_udev/' /etc/mkinitcpio.conf
+mkinitcpio -P
+
+MAC_ADDRESS=$(cat /sys/class/net/eth0/address)
+IPV4_ADDRESS=$(ip -4 addr show enp0s31f6  | grep inet | awk '{print $2}')
+IPV4_GATEWAY=$(ip -4 route show default | awk '{print $3}')
+IPV6_ADDRESS=$(ip -6 addr show | awk '/inet6/ {print $2}' | grep -v fe80 | grep -v ::1/128)
+IPV6_GATEWAY=$(ip -6 route show default | awk '{print $3}')
+
+cat > /etc/systemd/network/10-wired.network <<CONFIG
+[Match]
+Match=${MAC_ADDRESS}
+
+[Network]
+Address=${IPV4_ADDRESS}
+Gateway=${IPV4_GATEWAY}
+DNS=1.1.1.1
+DNS=8.8.8.8
+
+[Network]
+Address=${IPV6_ADDRESS}
+Gateway=${IPV6_GATEWAY}
+CONFIG
+
+echo "Here is your network config. Check for correctness."
+cat /etc/systemd/network/10-wired.network
+
+# Set the server's hostname.
+read -p "What is the server's hostname? " $hostname
+echo $hostname > /etc/hostname
+
+# Fix Archstrap's hostname in Bash:
+sed -i 's/\(PS1=.\+\)archstrap\(.\+\)/\1\\h\2/'  /etc/bash.bashrc
+
+systemctl enable systemd-networkd
+
+# Fix Archstrap's PS1:
+cat >> /etc/bash.bashrc <<BASH
+if [ $(id -u) -eq 0 ]; then 
+    export PS1='\[\033[01;31m\]\h \[\033[01;34m\]\W # \[\033[00m\]'; 
+else
+    export PS1='\[\]15:20.54\[\]\[\033[38;5;12m\][\[\]\[\033[38;5;10m\]xperion77\[\]\[\033[38;5;12m\]@\[\]\[\033[38;5;7m\]\h\[\]\[\033[38;5;12m\]]\[\]\[\033[38;5;15m\]: \[\]\[\033[38;5;7m\]\w\[\]\[\033[38;5;12m\]>\[\]\[\033[38;5;10m\]\$\[\]\[\033[38;5;15m\] \[\]'
+fi
+BASH
+
+echo "Adding $user to /etc/sudoers"
+sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
+echo "Installing yay (AUR)..."
+pacman -S --noconfirm base-devel
+cd /code
+setfacl -Rm d:g:users:rwX /code
+setfacl -Rm g:users:rwX /code
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+sudo -u ${user} makepkg -si
---
+++ install.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+curl -LO https://gist.githubusercontent.com/bhelm/65283c37a0cb585089041214002df4f7/raw/536099a5f969a6888c85e48a487ec19870c596a2/arch-chroot.sh
+chmod 0755 arch-chroot.sh
+mv arch-chroot.sh /usr/bin/arch-chroot
+
+sfdisk /dev/nvme0n1 < partition-table.sfdisk
+sfdisk /dev/nvme1n1 < partition-table.sfdisk
+
+# /
+mdadm --create --verbose /dev/md0 --level=0 --raid-devices=2 /dev/nvme0n1p2 /dev/nvme1n1p2
+# /var/lib/docker
+mdadm --create --verbose /dev/md1 --level=0 --raid-devices=2 /dev/nvme0n1p3 /dev/nvme1n1p3
+# /code /var/www /important
+mdadm --create --verbose /dev/md2 --level=1 --raid-devices=2 /dev/nvme0n1p4 /dev/nvme1n1p4
+# /storage
+mdadm --create --verbose /dev/md3 --level=1 --raid-devices=2 /dev/nvme0n1p5 /dev/nvme1n1p5
+
+# Format /boot
+mkfs.fat /dev/nvme0n1p1
+mkfs.fat /dev/nvme1n1p1
+
+# Format /
+mkfs.btrfs /dev/md0
+
+# Format /var/lib/docker
+mkfs.btrfs /dev/md1
+
+# Format /code /important /var/www
+mkfs.btrfs /dev/md2
+
+# Format /storage
+mkfs.xfs /dev/md3
+
+
+mount -o compress=lzo /dev/md0 /media
+cd /media
+btrfs subvolume create @boot
+btrfs subvolume create @rootfs
+btrfs subvolume create @snapshots
+
+cd -
+umount /media
+mount -o compress=lzo,subvol=@rootfs /dev/md0 /media
+
+cd /media
+mkdir code
+mount /dev/md2 code
+cd code
+btrfs subvolume create @code
+btrfs subvolume create @snapshots
+ls
+cd -
+umount /media/code
+mount -o subvol=@code /dev/md2 /media/code
+cd /media/code
+ls
+
+mkdir -p /media/boot
+mount -o subvol=@boot /dev/md0 /media/boot
+mkdir -p /media/boot/efi
+mount /dev/nvme0n1p1 /media/boot/efi
+
+## Resume
+mount -o compress=lzo,subvol=@rootfs /dev/md0 /media
+mount -o subvol=@code /dev/md2 /media/code
+mount /dev/nvme0n1p1 /media/boot/efi
+
+
+cd /media/code
+git clone https://github.com/wick3dr0se/archstrap; cd "${_##*/}"
+time ./archstrap /media
+
+cd /media
+rm -r archrootfs/boot/
+mv archrootfs/* .
+rm -r archrootfs/
+
+mkdir /media/var/lib/docker
+mount /dev/md1 /media/var/lib/docker
+
+mkdir /media/storage
+mount /dev/md3 /media/storage
+
+# Set default subvolume to @rootfs.
+btrfs subvolume set-default $(btrfs subvolume list /media | grep @rootfs | awk '{print $2}') /media
+
+mkdir -p /media/media/true-root
+mount -o subvol=/ /dev/md0 /media/media/true-root/
